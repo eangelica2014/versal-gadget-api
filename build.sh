9872298408e2a01a3f514ebe2193f9964d9197ed
Mike Bannister <notimpossiblemike@gmail.com>
2014-10-31 19:55:40 +0000

    Generated Fri Oct 31 19:55:40 UTC 2014 (c0e79df)

--- build.sh
+++ build.sh
@@ -5,14 +5,20 @@ set -o verbose
 
 rm -rf _site
 jekyll build
+echo 1 $PWD
 tmpdir=/tmp/$(date +%s)
 mkdir -p $tmpdir
 rm -rf $tmpdir/site
 cp -r _site $tmpdir/site
+echo 2 $PWD
 cd ..
+echo 3 $PWD
 git checkout gh-pages
+echo 4 $PWD
 rm -rf ./*
+echo 5 $PWD
 cp -r $tmpdir/site/* ./
+echo 6 $PWD
 git add -A
 COMMIT=$(git rev-parse --short HEAD)
 DATE=$(date)
